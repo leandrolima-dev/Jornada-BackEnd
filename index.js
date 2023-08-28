0a1eed5cc5b1caebde67c2eac71fa06f61df0e3f
Leandro Lima <lleandrorj@gmail.com>
2023-08-28 12:06:22 -0300

    create endpoint `oi`

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+//Endpoint principal
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(3000);
+//Endpoint /oi
+app.get("/oi", function (req, res) {
+    res.send("Olá, mundo!");
+  });
+
+app.listen(3000, () =>
+  console.log("Servidor rodando em http://localhost:3000/")
+);
