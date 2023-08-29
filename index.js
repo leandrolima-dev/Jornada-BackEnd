f850b3c0e7f8f90d193ce05085d64472feb299ac
Leandro Lima <lleandrorj@gmail.com>
2023-08-29 10:52:19 -0300

    create endpoints `GET` and `POST`

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const app = express();
 
+//Habilitamos o processamento de JSON
+app.use(express.json());
+
 //Endpoint principal
 app.get("/", function (req, res) {
   res.send("Hello, World");
 });
 
 //Endpoint /oi
-app.get("/oi", function (req, res) {
+app.get("/oi123", function (req, res) {
     res.send("Olá, mundo!");
   });
+
+// Endpoints de Herois
+const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+
+// Endpoint Read All -> [GET] /herois
+app.get("/herois", function (req, res) {
+  res.send(lista)
+});
+
+//Endpoint Create -> [POST] /herois
+app.post("/herois", function (req, res) {
+  console.log(req.body, typeof req.body);
+
+  //Extrai o nome do body da Request (corpo da requisição)
+  const item = req.body.nome;
+
+  //Inserir o item na lista
+  lista.push(item);
+
+  //Enviamos uma resposta de sucesso
+  res.send("Item criado com sucesso!")
+});
+
 
 app.listen(3000, () =>
   console.log("Servidor rodando em http://localhost:3000/")
