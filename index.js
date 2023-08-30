16a5b265e95814147aff27c0c0c918ba2c35d51a
Leandro Lima <lleandrorj@gmail.com>
2023-08-30 11:26:37 -0300

    dataBase integrate Read All `GET`

--- index.js
+++ index.js
@@ -1,78 +1,101 @@
+// Importando Express
 const express = require("express");
-const app = express();
+// Importando MongoDb
+const { MongoClient } = require("mongodb");
 
-//Habilitamos o processamento de JSON
-app.use(express.json());
+// Conexão URL
+// const url = "mongodb://localhost:27017";
+const url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
+// Database Name
+const dbName = "jornada-backend-agosto-23";
 
-//Endpoint principal
-app.get("/", function (req, res) {
-  res.send("Hello, World");
-});
+async function main() {
+  //Use o método connect para se conectar ao servidor
+  console.info("Conectando ao banco de dados...");
+  await client.connect();
+  console.info("Banco de dados conectado com sucesso!");
 
-//Endpoint /oi
-app.get("/oi", function (req, res) {
+  const db = client.db(dbName);
+  const collection = db.collection("herois");
+
+  const app = express();
+
+  //Habilitamos o processamento de JSON
+  app.use(express.json());
+
+  //Endpoint principal
+  app.get("/", function (req, res) {
+    res.send("Hello, World");
+  });
+
+  //Endpoint /oi
+  app.get("/oi", function (req, res) {
     res.send("Olá, mundo!");
   });
 
-// Endpoints de Herois
-const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+  // Endpoints de Herois
+  const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
 
-// Endpoint Read All -> [GET] /herois
-app.get("/herois", function (req, res) {
-  res.send(lista.filter(Boolean))
-});
+  // Endpoint Read All -> [GET] /herois
+  app.get("/herois", async function (req, res) {
+    const itens = await collection.find().toArray();
+    res.send(itens);
+  });
 
-//Endpoint Create -> [POST] /herois
-app.post("/herois", function (req, res) {
-  console.log(req.body, typeof req.body);
+  //Endpoint Create -> [POST] /herois
+  app.post("/herois", function (req, res) {
+    console.log(req.body, typeof req.body);
 
-  //Extrai o nome do body da Request (corpo da requisição)
-  const item = req.body.nome;
+    //Extrai o nome do body da Request (corpo da requisição)
+    const item = req.body.nome;
 
-  //Inserir o item na lista
-  lista.push(item);
+    //Inserir o item na lista
+    lista.push(item);
 
-  //Enviamos uma resposta de sucesso
-  res.send("Item criado com sucesso!")
-});
+    //Enviamos uma resposta de sucesso
+    res.send("Item criado com sucesso!");
+  });
 
-//Read By Id -> [GET] /herois/:id
-app.get("/herois/:id", function (req, res) {
-  //Pegamos o parâmetro de rota ID
-  const id = req.params.id - 1;
-
-  //Peagmos a informação da lista
-  const item = lista[id]
-
-  //Exibimos o item na pesquisa do endpoint
-  res.send(item);
-});
-
-//Update -> [PUT] /herois/:id
-app.put("/herois/:id", function (req, res) {
+  //Read By Id -> [GET] /herois/:id
+  app.get("/herois/:id", function (req, res) {
     //Pegamos o parâmetro de rota ID
-  const id = req.params.id - 1;
+    const id = req.params.id - 1;
 
-  //Extrai o nome do body da Request (corpo da requisição)
-  const item = req.body.nome;
+    //Peagmos a informação da lista
+    const item = lista[id];
 
-  //Atualizamos a informação na lista de registro
-  lista[id] = item;
+    //Exibimos o item na pesquisa do endpoint
+    res.send(item);
+  });
 
-  res.send("Item editado com sucesso");
-});
+  //Update -> [PUT] /herois/:id
+  app.put("/herois/:id", function (req, res) {
+    //Pegamos o parâmetro de rota ID
+    const id = req.params.id - 1;
 
-//Delete -> [DELETE] /herois/:id
-app.delete("/herois/:id", function (req, res) {
-  //Pegamos o parêmetro de rota Id
-  const id = req.params.id - 1;
-  
-  //Excluir o item da lista
-  delete lista[id];
+    //Extrai o nome do body da Request (corpo da requisição)
+    const item = req.body.nome;
 
-  res.send("Item excluído com sucesso!")
-})
+    //Atualizamos a informação na lista de registro
+    lista[id] = item;
 
-app.listen(3000, () =>
-  console.log("Servidor rodando em http://localhost:3000/")
-);
+    res.send("Item editado com sucesso");
+  });
+
+  //Delete -> [DELETE] /herois/:id
+  app.delete("/herois/:id", function (req, res) {
+    //Pegamos o parêmetro de rota Id
+    const id = req.params.id - 1;
+
+    //Excluir o item da lista
+    delete lista[id];
+
+    res.send("Item excluído com sucesso!");
+  });
+
+  app.listen(3000, () =>
+    console.log("Servidor rodando em http://localhost:3000/")
+  );
+}
+main();
